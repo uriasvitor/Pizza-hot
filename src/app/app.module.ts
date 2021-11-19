d48e4483320dcbf07aa90820b6c423b096e5c2c3
SAIRU <uriasvitor@gmail.com>
2021-11-18 23:03:24 -0300

    Add mount component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { HomeModule } from './components/home/home.module';
-import { NavigationModule } from './shared/components/navigation/navigation.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { mountComponent } from './components/mount/mount.component';
+import { sidebarComponent } from './components/mount/sidebars/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    mountComponent,
+    sidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/mount/mount.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "mount",
+    templateUrl: "./mount.component.html"
+})
+
+export class mountComponent implements OnInit{
+
+
+    ngOnInit(): void {
+        
+    }
+
+}
